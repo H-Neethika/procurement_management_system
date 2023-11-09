999bc3f8ffd9c7343b47a8fdee35f9d334406c85
ImashaNawodi <imashanaw1999@gmail.com>
2023-11-09 22:18:28 +0530

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import TopBar from './components/Topbar';
+import SideBar from './components/SideBar';
+
+
+ const App = () => {
+  return (
+    <div >
+     <TopBar/> 
+     <SideBar/>
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ frontend/src/components/TopBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/TopBar.css';
+
+export default function TopBar() {
+  return (
+    <div className="topBarStyle"> 
+      <img src="/Image/Picture1.png" alt="Logo" className="imageStyle" />
+      <div>
+        <h1 className="facultyTextStyle">FACULTY OF ENGINEERING UNIVERSITY OF RUHUNA</h1> 
+        <h2 className="systemTextStyle">PROCUREMENT MANAGEMENT SYSTEM</h2>
+      </div>
+      <button className="logoutButton">Logout</button>
+    </div>
+  );
+}
