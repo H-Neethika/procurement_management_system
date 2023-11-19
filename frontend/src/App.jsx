ecdaab4ca131cdcaea8354c71c68987137e1c15e
ImashaNawodi <imashanaw1999@gmail.com>
2023-11-19 09:31:24 +0530

    Admin account

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import TopBar from './components/Topbar';
 import SideBar from './components/SideBar';
+import LoginPage from './pages/LoginPage';
+import AdminAccount from './pages/AdminAccount';
 
 
 
- const App = () => {
+
+
+const App = () => {
   return (
-    <div >
-      <TopBar/>
-     <SideBar/>
+    <div>
+      <TopBar />
+      <SideBar />
+      <Routes>
+       { /*<Route path="/" element={<LoginPage />} /> */}
+       <Route path="/" element={<AdminAccount />} />
+        {/* Add more routes here if needed */}
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
